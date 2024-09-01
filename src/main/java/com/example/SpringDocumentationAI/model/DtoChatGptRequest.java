9c003a54c5a452a09653864691efad036101aaa5
lutencjusz <lutencjusz@gmail.com>
2024-09-01 12:05:39 +0200

    modify test for JWT

--- src/main/java/com/example/SpringDocumentationAI/model/DtoChatGptRequest.java
+++ src/main/java/com/example/SpringDocumentationAI/model/DtoChatGptRequest.java
@@ -1,13 +1,11 @@
 package com.example.SpringDocumentationAI.model;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 public class DtoChatGptRequest {
 
     private String id;

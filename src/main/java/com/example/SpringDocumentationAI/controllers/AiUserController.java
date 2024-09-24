ba8a6a08a90ce18dd3be54bc440efb4d3b67fe0b
lutencjusz <lutencjusz@gmail.com>
2024-09-24 21:32:23 +0200

    fix swagger

--- src/main/java/com/example/SpringDocumentationAI/controllers/AiUserController.java
+++ src/main/java/com/example/SpringDocumentationAI/controllers/AiUserController.java
@@ -9,12 +9,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 public class AiUserController {
 
--- src/main/java/com/example/SpringDocumentationAI/controllers/gui/SpringAssistantGuiController.java
+++ src/main/java/com/example/SpringDocumentationAI/controllers/gui/SpringAssistantGuiController.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -25,7 +24,6 @@ import java.util.Locale;
 
 @Slf4j
 @Controller
-@CrossOrigin
 public class SpringAssistantGuiController {
 
     @Autowired
